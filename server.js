1c4dd7f6efb045da8e623cef46d73d6aa5c1fc36
Timeness <careless@flash.co>
2025-02-15 07:11:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
+// Serve static files from the "public" directory
 app.use(express.static('public'));
+
+// Serve index.html on the root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
