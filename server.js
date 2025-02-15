a62d7eb29aa9cdc672187811556f3cc29c432442
Timeness <careless@flash.co>
2025-02-15 07:08:29 +0530

    done

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    socket.on('sendMessage', (data) => {
-        io.emit('receiveMessage', data);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
