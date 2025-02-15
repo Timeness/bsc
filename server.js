bb1921bf976d31892912bd48ae4b3fe98a5b9783
Timeness <careless@flash.co>
2025-02-15 07:20:32 +0530

    done

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const path = require('path');
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
